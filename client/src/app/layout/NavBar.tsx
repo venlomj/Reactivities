2a6ab7ea63c51ee3dd0b9a71c024ad4309d3a201
venlomj <venlo.mj@hotmail.nl>
2025-03-13 18:40:31 +0100

    End of section 5

---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,76 @@
+import { Group } from "@mui/icons-material";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  MenuItem,
+} from "@mui/material";
+
+type Props = {
+  openForm: () => void;
+}
+
+export default function NavBar({openForm}: Props) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundImage:
+            "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac)",
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box>
+              <MenuItem sx={{ display: "flex", gap: 2 }}>
+                <Group fontSize="large">
+                  <Typography variant="h4" fontWeight="bold">
+                    Reactivities
+                  </Typography>
+                </Group>
+              </MenuItem>
+            </Box>
+            <Box sx={{display: 'flex'}}>
+              <MenuItem
+                sx={{
+                  fontSize: "1.2rem",
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                }}
+              >
+                Activities
+              </MenuItem>
+              <MenuItem
+                sx={{
+                  fontSize: "1.2rem",
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                }}
+              >
+                About
+              </MenuItem>
+              <MenuItem
+                sx={{
+                  fontSize: "1.2rem",
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                }}
+              >
+                Contacts
+              </MenuItem>
+            </Box>
+            <Button size="large" variant="contained" color="warning"
+            onClick={openForm}
+            >
+              Create activity
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
+}
