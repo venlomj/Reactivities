1eea49ce75fcaeb11f95236c3352728feb2765ee
venlomj <venlo.mj@hotmail.nl>
2025-03-13 22:39:01 +0100

    End of section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,57 +1,20 @@
 import {
   Box,
   Container,
-  CssBaseline,
-  Typography
+  CssBaseline
 } from "@mui/material";
-import { useState } from "react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useActivities } from "../../lib/hooks/useActivities";
+import { Outlet } from "react-router";
+
 
 function App() {
-const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
-  const [editMode, setEditMode] = useState(false);
-  const {activities, isPending} = useActivities();
-
-  
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities!.find(x => x.id === id));
-  }
-
-  const handleCancelSelectActivity = () => {
-    setSelectedActivity(undefined);
-  }
-
-  const handleOpenForm = (id?: string) => {
-    if (id) handleSelectActivity(id)
-    else handleCancelSelectActivity()
-  setEditMode(true)  
-  }
-
-  const handleFormClose = () => {
-    setEditMode(false)
-  }
 
   return (
     <Box sx={{bgcolor: '#eee', minHeight: '100vh'}}>
       <CssBaseline />
-      <NavBar openForm={handleOpenForm} />
+      <NavBar />
       <Container maxWidth='xl' sx={{mt: 3}}>
-        {!activities || isPending ? (
-          <Typography>Loading...</Typography>
-        ): (
-          <ActivityDashboard
-         activities={activities}
-         selectActivity={handleSelectActivity}
-         cancelSelectActivity={handleCancelSelectActivity}
-         selectedActivity={selectedActivity}
-         editMode={editMode}
-         openForm={handleOpenForm}
-         closeForm={handleFormClose}
-         />
-        )}
+      <Outlet />
       </Container>
     </Box>
   );
--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,34 +1,58 @@
-import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material"
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  Typography,
+} from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import { Link } from "react-router";
 
 type Props = {
-    activity: Activity
-    selectActivity: (id: string) => void;
-  }
+  activity: Activity;
+};
 
-export default function ActivityCard({activity, selectActivity}: Props) {
-  const {deleteActivity} = useActivities();
+export default function ActivityCard({ activity }: Props) {
+  const { deleteActivity } = useActivities();
 
   return (
-    <Card sx={{borderRadius: 3}}>
-        <CardContent>
-            <Typography variant="h5">{activity.title}</Typography>
-            <Typography sx={{color: 'text-secondary', mb: 1}}>{activity.date}</Typography>
-            <Typography variant="body2">{activity.description}</Typography>
-            <Typography variant="subtitle1">{activity.city} / {activity.venue}</Typography>
-        </CardContent>
-        <CardActions sx={{display: 'flex', justifyContent: 'space-between', pb:2}}>
-            <Chip label={activity.category} variant="outlined" />
-            <Box display='flex' gap={3}>
-            <Button onClick={() => selectActivity(activity.id)} size="medium" variant="contained">View</Button>
-            <Button
-             onClick={() => deleteActivity.mutate(activity.id)} 
-             disabled={deleteActivity.isPending}
-             color="error"
-             size="medium" 
-             variant="contained">Delete</Button>
-            </Box>
-        </CardActions>
+    <Card sx={{ borderRadius: 3 }}>
+      <CardContent>
+        <Typography variant="h5">{activity.title}</Typography>
+        <Typography sx={{ color: "text-secondary", mb: 1 }}>
+          {activity.date}
+        </Typography>
+        <Typography variant="body2">{activity.description}</Typography>
+        <Typography variant="subtitle1">
+          {activity.city} / {activity.venue}
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
+      >
+        <Chip label={activity.category} variant="outlined" />
+        <Box display="flex" gap={3}>
+          <Button
+            component={Link}
+            to={`/activities/${activity.id}`}
+            size="medium"
+            variant="contained"
+          >
+            View
+          </Button>
+          <Button
+            onClick={() => deleteActivity.mutate(activity.id)}
+            disabled={deleteActivity.isPending}
+            color="error"
+            size="medium"
+            variant="contained"
+          >
+            Delete
+          </Button>
+        </Box>
+      </CardActions>
     </Card>
-  )
+  );
 }
